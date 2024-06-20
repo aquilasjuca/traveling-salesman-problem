5124d75b7d841805b3bb991b6d816f1212059c1d
Kevin439560 <kevin.freitas560@gmail.com>
2024-06-20 10:49:24 -0300

    Têmpera Simulada implementada(passível de mudanças)
    O algoritmo de otimização foi implementado, mas pode ser necessário realizar ajustes e validações

---
+++ algorithms/simulated_annealing.py
@@ -0,0 +1,46 @@
+import random
+import math
+from models.traveling_salesman import TravelingSalesman
+
+class SimulatedAnnealing:
+    def __init__(self, tsp, initial_temp=1000, cooling_rate=0.995, min_temp=1):
+        self.tsp = tsp
+        self.initial_temp = initial_temp
+        self.cooling_rate = cooling_rate
+        self.min_temp = min_temp
+
+    def accept_probability(self, old_cost, new_cost, temperature):
+        if new_cost < old_cost:
+            return 1.0
+        return math.exp((old_cost - new_cost) / temperature)
+
+    def two_opt_swap(self, tour, i, j):
+        new_tour = tour[:]
+        new_tour[i:j+1] = reversed(tour[i:j+1])
+        return new_tour
+
+    def optimize(self):
+        n = len(self.tsp.cities)
+        current_tour = list(range(n))
+        random.shuffle(current_tour)
+        current_distance = self.tsp.distance(current_tour)
+        best_tour = current_tour[:]
+        best_distance = current_distance
+
+        temperature = self.initial_temp
+        while temperature > self.min_temp:
+            i, j = sorted(random.sample(range(n), 2))
+            new_tour = self.two_opt_swap(current_tour, i, j)
+            new_distance = self.tsp.distance(new_tour)
+
+            if self.accept_probability(current_distance, new_distance, temperature) > random.random():
+                current_tour = new_tour
+                current_distance = new_distance
+
+                if current_distance < best_distance:
+                    best_tour = current_tour[:]
+                    best_distance = current_distance
+
+            temperature *= self.cooling_rate
+
+        return best_tour, best_distance
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from models.traveling_salesman import TravelingSalesman
 from algorithms.hill_climbing import HillClimbing
 from file_management.TSPFileReader import *
 # from algorithms.genetic_algorithm import GeneticAlgorithm
-# from algorithms.simulated_annealing import SimulatedAnnealing
+from algorithms.simulated_annealing import SimulatedAnnealing
 
 class TSPSolver:
     def __init__(self, cities):
@@ -16,40 +16,35 @@ class TSPSolver:
     #     genetic_algorithm = GeneticAlgorithm(self.tsp)
     #     return genetic_algorithm.optimize()
 
-    # def solve_with_simulated_annealing(self):
-    #     simulated_annealing = SimulatedAnnealing(self.tsp)
-    #     return simulated_annealing.optimize()
+    def solve_with_simulated_annealing(self):
+        simulated_annealing = SimulatedAnnealing(self.tsp)
+        return simulated_annealing.optimize()
 
 def main():
-    cities = [
-        [0, 10, 15, 20],
-        [10, 0, 35, 25],
-        [15, 35, 0, 30],
-        [20, 25, 30, 0]
-    ]
+
     archive_name = 'distance.txt'
     dist_reader = TSPFileReader(archive_name)
     dist_reader.read_file()
 
     for line in dist_reader.get_dist_mat():
         print(line)
-
+    
     solver = TSPSolver(dist_reader.get_dist_mat())
 
     print("Hill Climbing:")
     tour, dist = solver.solve_with_hill_climbing()
     print("Melhor rota:", tour)
     print("Distância:", dist)
-
+    
     # print("\nAlgoritmo Genético:")
     # tour, dist = solver.solve_with_genetic_algorithm()
     # print("Melhor rota:", tour)
     # print("Distância:", dist)
 
-    # print("\nTêmpera Simulada:")
-    # tour, dist = solver.solve_with_simulated_annealing()
-    # print("Melhor rota:", tour)
-    # print("Distância:", dist)
+    print("\nTêmpera Simulada:")
+    tour, dist = solver.solve_with_simulated_annealing()
+    print("Melhor rota:", tour)
+    print("Distância:", dist)
 
 if __name__ == "__main__":
     main()
